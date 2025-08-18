dc9b34832178f5dfa43762309d0a5a66da07db99
Hozaifa Bin Masud <hozaifabinmasud@gmail.com>
2025-08-18 21:50:16 +0600

    fix root path

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const dbConnect = require("./db");
 const morgan = require("morgan");
 
+// try to update
+
 // Final test of custom path filter - backend only
 dotenv.config();
 const app = express();
